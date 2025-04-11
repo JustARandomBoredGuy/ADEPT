23989ab9059e144451966db4bce583d8a70aea11
JustARandomBoredGuy <akgupta.pro@gmail.com>
2025-04-11 20:19:25 +0530

    Added saving pdf endpoint

--- backend/src/controllers/api.controller.ts
+++ backend/src/controllers/api.controller.ts
@@ -1,12 +1,23 @@
+import path from "path";
 import { BAD_REQUEST, OK } from "../constants/http";
 import appAssert from "../utils/appAssert";
 import catchErrors from "../utils/catchErrors";
+import fs from "fs";
 
 
+const pdfDir = "./src/constants/rawData"
+type fileSchema = Express.Multer.File[]
 export const pdfHandler = catchErrors(async (req, res) => {
-    const file = req.files;
-    appAssert(file, BAD_REQUEST, "File not found in request")
+    const files = req.files as fileSchema
+    appAssert(files, BAD_REQUEST, "No files sent")
+    console.log(files)
 
-    console.log(file)
+    for (const file of files) {
+        const filename = `${file.originalname}`;
+        const filePath = path.join(pdfDir, filename);
+    
+        fs.writeFileSync(filePath, file.buffer);
+      }
+
     return res.status(OK).json({ message: "File received successfully "})
 })
