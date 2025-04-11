e00162c25a7c9c377a0e4e32e4e5bc715f80883f
JustARandomBoredGuy <akgupta.pro@gmail.com>
2025-04-11 18:22:32 +0530

    reformatting

--- backend/src/controllers/api.controller.ts
+++ backend/src/controllers/api.controller.ts
@@ -1,13 +1,12 @@
-import { NOT_FOUND, OK } from "../constants/http";
+import { BAD_REQUEST, OK } from "../constants/http";
 import appAssert from "../utils/appAssert";
 import catchErrors from "../utils/catchErrors";
 
 
 export const pdfHandler = catchErrors(async (req, res) => {
     const file = req.files;
-    appAssert(file, NOT_FOUND, "File not found in request")
-    
-    console.log(file)
+    appAssert(file, BAD_REQUEST, "File not found in request")
 
+    console.log(file)
     return res.status(OK).json({ message: "File received successfully "})
 })
