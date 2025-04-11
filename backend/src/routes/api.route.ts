d23dccdb9b85be664a1763ff97331c58fdb9bfda
JustARandomBoredGuy <akgupta.pro@gmail.com>
2025-04-11 17:11:47 +0530

    Basic express backend

---
+++ backend/src/routes/api.route.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+const apiRoutes = Router();
+
+// prefix: /api
+// apiRoutes.post("/parsePDF", pdfHandler);
+// apiRoutes.post("/parseLink", linkHandler);
+
+export default apiRoutes;
